2496e04d77a6a3fdbe03ab2ed33a346b688d401d
Trucho <Trucho@LAPTOP-TL580T63>
2024-11-15 22:24:44 -0600

    pruba

---
+++ src/main/java/com/anahuac/mayab/modulo1/sesion3/TestCartilla2.java
@@ -0,0 +1,21 @@
+package com.anahuac.mayab.modulo1.sesion3;
+
+public class TestCartilla2 {
+
+		public static void main(String[] args) {
+			Perro miPerro = new Perro( "unica", "Firulais", 5, "Paty");
+			CartillaPerro cartilla = new CartillaPerro();
+			cartilla.setDatosPerro(miPerro);
+			cartilla.actualizar("rabia", "5-marzo-2022");
+			cartilla.actualizar("Parvo", "5-marzo-2022");
+			cartilla.actualizar("rabia2", "5-marzo-2022");
+			cartilla.actualizar("Moc", "5-marzo-2022");
+			
+		
+		cartilla.imprimirCartilla();
+		cartilla.imprimirExpediente();
+		}
+
+	}
+
+}
